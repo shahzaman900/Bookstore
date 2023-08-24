d6e6f45143f614fab4d5d684bbb052a41bcbd07c
sheezu900 <shahzamanlive2@gmail.com>
2023-08-24 16:05:11 +0500

    install redux and add folder and files

---
+++ src/redux/dist/store.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _toolkit = require("@reduxjs/toolkit");
+
+var store = (0, _toolkit.configureStore)({
+  reducer: {}
+});
+var _default = store;
+exports["default"] = _default;
