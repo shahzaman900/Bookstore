02cbd7dc8059a6a8bfe6f6c95aabd88ddb2d2a97
sheezu900 <shahzamanlive2@gmail.com>
2023-08-25 15:50:59 +0500

    add delete and add button

--- src/components/displaybook.js
+++ src/components/displaybook.js
@@ -1,5 +1,9 @@
 /* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../redux/books/booksSlice';
+
 export default function Book({ book }) {
+  const dispatch = useDispatch();
   return (
     <>
       <h1>{ book.title }</h1>
@@ -8,7 +12,8 @@ export default function Book({ book }) {
         <strong>Auther: </strong>
         { book.author }
       </p>
-      <button type="button">Delete</button>
+      <button type="button" onClick={dispatch(addBook())}>Delete</button>
+      <button type="button" onClick={dispatch(removeBook())}>Add</button>
     </>
   );
 }
